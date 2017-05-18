867637a0a13dbf8e663f4f631d725076126fdf31
TSturtledove <roberttortorelli@yahoo.com>
2017-05-18 15:09:52 -0500

    commented out a })

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use( (req, res, next) => {
 // app.use( (req, res, next) => {
 //   console.log("session?", req.session)
 //   next()
-})
+// })
 app.use(express.static("public"))
 app.use("/api/v1/", routes);
 
